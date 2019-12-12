b396332c6b8cad1575ba27b0db6cef2d3773c27d
Tomasz Zwierzyński <tomasz0zwierzynski@gmail.com>
2019-12-12 19:05:44 +0100

    Dodano serwer socketio

--- dist/app.js
+++ dist/app.js
@@ -4,15 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = __importDefault(require("socket.io"));
 const app = express_1.default();
+const server = http_1.default.createServer(app);
 const port = 3000;
 app.get('/', (req, res) => {
     res.send('The sedulous hyena ate the antelope!');
 });
-app.listen(port, err => {
-    if (err) {
-        return console.error(err);
-    }
+socket_io_1.default(http_1.default).on('connection', function (socket) {
+    console.log('a user connected');
+});
+server.listen(port, function () {
     return console.log(`server is listening on ${port}`);
 });
 //# sourceMappingURL=app.js.map
