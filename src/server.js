53b53b2de31e77b36446befb09858be091d0706b
tzwierzy <T.Zwierzynski@wasko.pl>
2019-12-13 13:55:08 +0100

    Dodano do serwera

--- src/server.js
+++ src/server.js
@@ -3,14 +3,34 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-io.on('connection', function (socket){
-   console.log('connection');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+let socketPi;
+io.on('connection', function (socket){
+  console.log(socket);
+
+	socketPi = socket;
+  
   socket.on('CH01', function (from, msg) {
     console.log('MSG', from, ' saying ', msg);
   });
 
 });
+
+app.get( '/', function (req, res) { 
+	res.send( 'testEndpoint' );
+} );
+
+app.post( '/send', function ( req, res ) {
+	console.log(req);
+	console.log(req.body);
+	if (socketPi) {
+		socketPi.emit('CH02', req.body);
+	}
+	res.send('OK');
+} );	
 
 http.listen(3000, function () {
   console.log('listening on *:3000');
